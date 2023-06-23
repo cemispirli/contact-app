f1360c44fa7f22ae9f9783ed6b532e37cf7d88f9
cemispirli <cemispirli08@gmail.com>
2023-06-23 21:43:35 +0300

    fixed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,15 +3,23 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   
-
-apiKey: process.env.REACT_APP_API_KEY,
-authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-databaseURL: process.env.REACT_APP_DATABASE_URL,
-projectId: process.env.REACT_APP_PROJECT_ID,
-storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-appId: process.env.REACT_APP_APP_ID,
-measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    apiKey: "AIzaSyDQdvj3FCNFGt4oIDiL0cZw9bjp3UbfpNI",
+    authDomain: "contact-2524.firebaseapp.com",
+    databaseURL: "https://contact-2524-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "contact-2524",
+    storageBucket: "contact-2524.appspot.com",
+    messagingSenderId: "97622436500",
+    appId: "1:97622436500:web:683f94a14f7e8bba5e6281",
+    measurementId: "G-LZ204M64K0"
+    
+// apiKey: process.env.REACT_APP_API_KEY,
+// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// databaseURL: process.env.REACT_APP_DATABASE_URL,
+// projectId: process.env.REACT_APP_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_APP_ID,
+// measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 
 };
 
